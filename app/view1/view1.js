9c791dd7d094ff4673477acd963a515dae487750
Neil Tomlin <neil.tomlin@LC-LTW025.local>
2016-11-08 10:42:48 +0000

    some more work

--- app/view1/view1.js
+++ app/view1/view1.js
@@ -5,10 +5,13 @@ angular.module('myApp.view1', ['ngRoute'])
 .config(['$routeProvider', function($routeProvider) {
   $routeProvider.when('/view1', {
     templateUrl: 'view1/view1.html',
-    controller: 'View1Ctrl'
+    controller: 'View1Ctrl',
+    controllerAs: 'v1'
   });
 }])
 
 .controller('View1Ctrl', [function() {
-
+  this.formEntry = {title: "", name: "", type: ""};
+  this.showModal = false;
+  this.testtext = "hello";
 }]);
